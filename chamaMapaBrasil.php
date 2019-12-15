bc28f7474f9b957408c61d5e500c08db3fa90f50
kaiomudkt <kaiomudkt@gmail.com>
2019-12-15 16:36:30 -0400

    escrevendo readme.md

---
+++ chamaMapaBrasil.php
@@ -0,0 +1,22 @@
+<div>
+    <div id="mapa_brasil">
+        <?php
+            include('Mapa+do+Brasil+SVGa.html');
+        ?>
+    </div>
+
+    <div id="lista_pet_do_estado">
+        <?php 
+            /*listar todos os pets desse estado*/
+        ?>
+        
+        <ul>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+        </ul>
+    </div>
+</div>
