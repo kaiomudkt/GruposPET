d1c6e26b32339a4b32624a1aefe40349e14161e6
kaiomudkt <kaiomudkt@gmail.com>
2019-12-15 22:55:33 -0400

    coloquei o mapa dentro do arquivo home-callout.php

---
+++ home-callout.php
@@ -0,0 +1,27 @@
+	
+<!-- MAPA BRASIL -->
+<div id="mapa_pets_brasil">
+    <div style="">
+        <div id="mapa_brasil">
+        <?php
+            include('Mapa+do+Brasil+SVGa.html');
+        ?>
+    </div>
+
+    </div>
+
+    <div id="lista_pet_estado">
+        <?php 
+            /*listar todos os pets desse estado*/
+        ?>
+        
+        <ul>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+                <li>PET Sistemas UFMS FACOM</li>
+        </ul>
+    </div>
+</div>
