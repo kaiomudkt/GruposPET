8059fee5996cd765eb433f496c8e57a10688d12d
kaiomudkt <kaiomudkt@gmail.com>
2020-03-14 17:34:43 -0400

    trabalhando no arquivo Mapa+do+Brasil+svg.html na requisicao do json dos PETs do Brasil

--- home-callout.php
+++ home-callout.php
@@ -7,6 +7,7 @@
                 include('Mapa+do+Brasil+SVGa.html');
             ?>
         </div>
+
     </div>
 
     <div id="lista_pet_estado">
@@ -21,43 +22,46 @@
             ainda falta fazer o desinger, ou aproveitar o designer do "home-blog.php" do tema "Kyma"
             */
 
-
-
             //https://developer.wordpress.org/reference/functions/rest_url/
             //https://github.com/WP-API/client-js/blob/master/client-js.php
             //https://github.com/oskarrough/ember-wordpress
             //https://codex.wordpress.org/Function_Reference/wp_remote_get
             //https://codex.wordpress.org/Function_Reference/register_post_type
 
-            $arguments = array('method' => 'GET');
-            $url = 'http://localhost:8083/?rest_route=/wp/v2/pets';
-            
+            try {
 
-            // Faz a solicitação GET para o endereço.
-            $request = wp_remote_get( $url, $arguments);
-            // Se não houve erro...
-            if ( ! is_wp_error( $request ) ) {
-                // pegamos o "corpo" da resposta recebida...
-                $body = wp_remote_retrieve_body( $request );
-                // e transformamos de JSON em um array PHP normal.
-                $data = json_decode( $body );
-            
-                // Se não houve erro nesta etapa, iteramos pelo array
-                // e montamos uma lista com título e link.
-                if ( ! is_wp_error( $data ) ) {
-                    echo '<ul>';
-                    foreach( $data as $rest_post ) {
-                        echo '<li>';
+                $arguments = array('method' => 'GET');
+                $url = 'http://grupospet.ufms.br/?rest_route=/wp/v2/pet_post_type_key';
+                
+                // Faz a solicitação GET para o endereço.
+                $request = wp_remote_get( $url, $arguments);
+                echo is_wp_error( $request );
+                // Se não houve erro...
+                if ( is_wp_error( $request ) < 1) {
+                    // pegamos o "corpo" da resposta recebida...
+                    $body = wp_remote_retrieve_body( $request );
+                    // e transformamos de JSON em um array PHP normal.
+                    $data = json_decode( $body );
+                    
+                    // Se não houve erro nesta etapa, iteramos pelo array
+                    // e montamos uma lista com título e link.
+                    if ( ! is_wp_error( $data ) ) {
+                        echo '<ul>';
+                        foreach( $data as $rest_post ) {
+                            echo '<li>';
                             echo '<a href="' . esc_url( $rest_post->link ) . '">' . $rest_post->title->rendered . '</a>';
-                        echo '</li>';
+                            echo '</li>';
+                        }
+                        echo '</ul>';
+                    }else{
+                        echo 'erro: is_wp_error( $data)';
                     }
-                    echo '</ul>';
                 }else{
-                    echo 'erro: is_wp_error( $data)';
+                    $error_msg = $request->get_error_message();
+                    echo "error: request = wp_remote_get(): $error_msg";
                 }
-            }else{
-                $error_msg = $request->get_error_message();
-                echo "error: request = wp_remote_get(): $error_msg";
+            }catch(Exception $e) {
+                echo $e->getMessage();
             }
         ?>
     </div>
